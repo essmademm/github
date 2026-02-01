e625fdc70728ddebb8210649180fe114c3a9f858
esmasamm <twenty2one1kg@gmail.com>
2026-02-01 22:21:22 +0600

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -127,6 +127,141 @@ public class Main {
 
 
 
+//////////////11. Find Common Elements (Countries)
+        ArrayList<String> l1 = new ArrayList<>(Arrays.asList(
+                "USA", "KR", "Turkie", "KZ"));
+        ArrayList<String> l2 = new ArrayList<>(Arrays.asList(
+                "KR", "Germany", "KZ"));
+
+        l1.retainAll(l2);
+
+        System.out.println("Common coun: " + l1);
+
+
+
+/// /////////////12. Remove Even-Length Strings (Names)
+//
+        ArrayList<String> names = new ArrayList<>(Arrays.asList(
+                "Samira", "Jonny", "Asil", "Nur"));
+
+        names.removeIf(name -> name.length() % 2 == 0);
+
+        System.out.println(names);
+
+
+
+/// //////////////////////3. Find Smallest Element (Shortest Song)
+        ArrayList<String> songs = new ArrayList<>(Arrays.asList(
+                "Stay", "Halazia", "Work"));
+
+        String shor = songs.get(0);
+
+        for (String song : songs) {
+            if (song.length() < shor.length()) {
+                shor = song;
+            }
+        }
+
+        System.out.println("Shortest song: " + shor);
+
+
+
+/// ////14. Replace All Vowels with "*"
+
+        ArrayList<String> words = new ArrayList<>(Arrays.asList(
+                "infinitive", "insomnia", "array"));
+
+        for (int i = 0; i < words.size(); i++) {
+            words.set(i, words.get(i).replaceAll("[aeiou]", "*"));
+        }
+
+        System.out.println(words);
+
+
+
+
+        /// //////////5. Partition ArrayList (Even & Odd Numbers)
+
+        ArrayList<Integer> numberss = new ArrayList<>(Arrays.asList(
+                1, 2, 3, 4, 5, 6));
+
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+
+        for (int num : numberss) {
+            if (num % 2 == 0)
+                even.add(num);
+            else
+                odd.add(num);
+        }
+
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+
+
+
+
+        /// ////////16. Rotate Elements by 2 Positions (Days)
+
+        ArrayList<String> days = new ArrayList<>(Arrays.asList(
+                "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"));
+
+        Collections.rotate(days, 2);
+
+        System.out.println(days);
+
+
+
+        ///////// 17. Remove Null Values (Student Names)
+        ArrayList<String> students = new ArrayList<>(Arrays.asList(
+                "Akim", null, "Islam", null, "Aruu"));
+
+        students.removeIf(Objects::isNull);
+
+        System.out.println(students);
+
+
+
+
+/// ////////////////18. Find Second Largest String (by Length)
+
+        ArrayList<String> movie = new ArrayList<>(Arrays.asList(
+                "star", "mountains", "Masha", "Avatar"));
+
+        movies.sort((z, c) -> c.length() - z.length());
+
+        System.out.println("2. largest: " + movie.get(1));
+
+
+/// ///////////////19. Replace Elements with Lengths (Fruits)
+
+        ArrayList<String> fruits = new ArrayList<>(Arrays.asList(
+                "Apple", "Banan", "Kiwi"));
+
+        ArrayList<Integer> lengths = new ArrayList<>();
+
+        for (String fruit : fruits) {
+            lengths.add(fruit.length());
+        }
+
+        System.out.println(lengths);
+
+
+
+
+
+
+        /// //////////20. Create Nested ArrayList (Departments)
+
+        ArrayList<ArrayList<String>> departments = new ArrayList<>();
+
+        ArrayList<String> it = new ArrayList<>(Arrays.asList("Akim", "Baias"));
+        ArrayList<String> hr = new ArrayList<>(Arrays.asList("Samira", "Joni"));
+
+        departments.add(it);
+        departments.add(hr);
+
+        System.out.println(departments);
 
 
 
